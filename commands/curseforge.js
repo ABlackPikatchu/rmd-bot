2272eb60d517d563ab2bfc74d96ab969ba3c6cca
matyrobbrt <robertmateimoraru@gmail.com>
2021-09-04 20:02:37 +0300

    CF command

---
+++ commands/curseforge.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'curseforge',
+    description: 'Curseforge info',
+    execute(msg, args) {
+        msg.reply(
+        "Currently the mod is not published on CF, but you can check the GitHub!"
+        );
+    }
+};
--- commands/index.js
+++ commands/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     Ping: require('./ping'),
-    Github: require('./github')
+    Github: require('./github'),
+    CurseForge: require('./curseforge')
   };
