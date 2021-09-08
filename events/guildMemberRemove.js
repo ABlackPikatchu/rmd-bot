6bbf5a7ac9a7169db9142199dc12d39d0cfd42f4
matyrobbrt <robertmateimoraru@gmail.com>
2021-09-08 15:38:01 +0300

    now the bot stores user's roles when they leave
    no way to get rid of mutes

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,13 +1,21 @@
 const { MessageEmbed } = require('discord.js');
+const db = require('quick.db')
 module.exports = {
-	name: 'guildMemberRemove',
-	once: false,
-	execute(member) {
-        const goodbyeEmbed = new MessageEmbed();
+        name: 'guildMemberRemove',
+        once: false,
+        async execute(member) {
+                let rolesList = [];
+                member.roles.cache.forEach(role => {
+                        if (role.rawPosition === 0) return; 
+                        rolesList.push(role.name);
+                })
 
-        goodbyeEmbed.setColor('#f00000')
-        goodbyeEmbed.setTitle('**' + member.user.username + '** left us! There are now **' + member.guild.memberCount + '** left Among Us!')
-      
-        member.guild.channels.cache.find(i => i.name === '👋︱welcome').send({ embeds: [goodbyeEmbed] });
-	},
+                db.set(`roles_${member.id}`, rolesList);
+                const goodbyeEmbed = new MessageEmbed();
+
+                goodbyeEmbed.setColor('#f00000')
+                goodbyeEmbed.setTitle('**' + member.user.username + '** left us! There are now **' + member.guild.memberCount + '** left Among Us!')
+
+                member.guild.channels.cache.find(i => i.name === '👋︱welcome').send({ embeds: [goodbyeEmbed] });
+        },
 };
