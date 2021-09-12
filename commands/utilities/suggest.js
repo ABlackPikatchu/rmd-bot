c2a4d497e3f5482bbc297e7c8124ad067dadbfbf
matyrobbrt <robertmateimoraru@gmail.com>
2021-09-12 19:56:07 +0300

    updated suggest command

--- commands/utilities/suggest.js
+++ commands/utilities/suggest.js
@@ -1,16 +1,44 @@
 const { MessageEmbed, Permissions } = require('discord.js');
+const channelsJSON = require('../../JSON/channels.json');
+
 const aliases = ['suggestion'];
+const description = 'Sends a suggestion';
+const helpEmbed = new MessageEmbed()
+    .setTitle("How to use the **SUGGEST** command!")
+    .setColor('RANDOM')
+    .setDescription(description)
+    .addFields(
+        { name: "Name", value: "suggest", inline: false },
+        { name: "Aliases", value: aliases.toString() + "\n", inline: false },
+        { name: "Usage", value: "suggest ***type suggestion***", inline: false },
+        { name: 'Permissions/Roles Required', value: 'none', inline: false },
+        {
+            name: "Arguments", value:
+                "**type** - the type of the suggestion. Can be **mod** or **server** (mandatory)\n" +
+                "**suggestion** - the suggestion (e.g. Add the machine: blah) (mandatory)\n"
+            , inline: false
+        }
+    );
+
 module.exports = {
     name: 'suggest',
     aliases: aliases,
-    description: 'Sends a suggestion',
+    description: description,
+    helpEmbed: helpEmbed,
     execute(message, args, bot) {
         const type = args.shift().toLowerCase();
         if (type === 'mod') {
-            const modChannel = message.guild.channels.cache.find(i => i.name === '🎮︱mod-suggestions');
+            const modChannel = message.guild.channels.cache.get(channelsJSON.mod_suggestions);
 
             if (!args[0]) return message.channel.send('You need to enter a Suggestion');
-            message.channel.send(`Thank you <@${message.author.id}> for giving the suggestion of: \n${args.join(' ')}`);
+            
+            const thanksEmbed1 = new MessageEmbed()
+                .setTitle(`Thank you for your mod suggestion!`)
+                .setDescription(`Thank you <@${message.author.id}> for the suggestion! Check it out in <#${channelsJSON.mod_suggestions}>!`)
+                .addField(`Content:`, `${args.join(' ')}`, false)
+                .setColor(`GREEN`);
+
+            message.reply({embeds: [thanksEmbed1]})
 
             const modEmbed = new MessageEmbed()
                 .setTitle(`New Suggestion`)
@@ -29,10 +57,17 @@ module.exports = {
                     console.log("❌could not react to the message!");
                 });
         } else if (type === 'server') {
-            const serverChannel = message.guild.channels.cache.find(i => i.name === '🚃︱server-suggestions');
+            const serverChannel = message.guild.channels.cache.get(channelsJSON.server_suggestions);
 
             if (!args[0]) return message.channel.send('You need to enter a Suggestion');
-            message.channel.send(`Thank you <@${message.author.id}> for giving the suggestion of: \n${args.join(' ')}`);
+
+            const thanksEmbed2 = new MessageEmbed()
+                .setTitle(`Thank you for your server suggestion!`)
+                .setDescription(`Thank you <@${message.author.id}> for the suggestion! Check it out in <#${channelsJSON.server_suggestions}>!`)
+                .addField(`Content:`, `${args.join(' ')}`, false)
+                .setColor(`GREEN`);
+
+            message.reply({embeds: [thanksEmbed2]})
 
             const serverEmbed = new MessageEmbed()
                 .setTitle(`New Suggestion`)
